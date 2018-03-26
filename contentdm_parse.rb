647cfa387ec1027aa487e4e39b5a9a9b359bb52d
Andrew Weaver <weevz@uw.edu>
2018-03-26 16:47:23 -0700

    startcsv

--- contentdm_parse.rb
+++ contentdm_parse.rb
@@ -1,9 +1,23 @@
 require 'csv'
+require 'fileutils'
 
 InputCSV = ARGV[0]
 InputDIR = ARGV[1]
-CSV.foreach(InputCSV,col_sep: "\t", quote_char: "ア",headers: true) do |row|
-  id = File.basename(row['Identifier'])
-  id_path = Dir.glob("#{InputDIR}/**/#{id}")
-  puts id_path
+DesinationDIR = ARGV[2]
+CSV.open("#{DesinationDIR}/metadata.csv", "wb") do |csv|
+  CSV.foreach(InputCSV,col_sep: "\t", quote_char: "ア",headers: true) do |row|
+    id = File.basename(row['Identifier'])
+    id_path = Dir.glob("#{InputDIR}/**/#{id}")
+    if ! id_path.empty?
+      filesearch = id_path
+      filesearch.each do |file|
+        FileUtils.cp file, DesinationDIR + '/' + File.basename(file)
+      end
+
+    else
+      filesearch = 'Path not found'
+    end
+    row ["Path"] = filesearch
+    csv << row
+  end
 end
