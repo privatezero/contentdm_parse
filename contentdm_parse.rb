bf692dea793454878de476be4e23aa239ad3116a
Andrew Weaver <weevz@uw.edu>
2018-03-26 15:34:38 -0700

    start to find paths

--- contentdm_parse.rb
+++ contentdm_parse.rb
@@ -1,4 +1,9 @@
 require 'csv'
 
-Input = ARGV[0]
-var = CSV.read(Input,col_sep: "\t", quote_char: "ア",headers: true)
+InputCSV = ARGV[0]
+InputDIR = ARGV[1]
+CSV.foreach(InputCSV,col_sep: "\t", quote_char: "ア",headers: true) do |row|
+  id = File.basename(row['Identifier'])
+  id_path = Dir.glob("#{InputDIR}/**/#{id}")
+  puts id_path
+end
