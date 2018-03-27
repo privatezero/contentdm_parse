2872e6f805f95694469ece76cbe391fbd3e45e25
Andrew Weaver <weevz@uw.edu>
2018-03-27 15:32:09 -0700

    add headers to output

--- contentdm_parse.rb
+++ contentdm_parse.rb
@@ -6,18 +6,22 @@ InputDIR = ARGV[1]
 DesinationDIR = ARGV[2]
 CSV.open("#{DesinationDIR}/metadata.csv", "wb") do |csv|
   CSV.foreach(InputCSV,col_sep: "\t", quote_char: "ア",headers: true) do |row|
-    id = File.basename(row['Identifier'])
-    id_path = Dir.glob("#{InputDIR}/**/#{id}")
-    if ! id_path.empty?
-      filesearch = id_path
-      filesearch.each do |file|
-        FileUtils.cp file, DesinationDIR + '/' + File.basename(file)
+    if ! row['Identifier'].nil?
+      id = File.basename(row['Identifier'])
+      id_path = Dir.glob("#{InputDIR}/**/#{id}")
+      if ! id_path.empty?
+        filesearch = id_path
+        filesearch.each do |file|
+          FileUtils.cp file, DesinationDIR + '/' + File.basename(file)
+        end
+      else
+        filesearch = 'Path not found'
       end
-
-    else
-      filesearch = 'Path not found'
     end
     row ["Path"] = filesearch
+    if $. == 2
+      csv << row.headers
+    end
     csv << row
   end
 end
