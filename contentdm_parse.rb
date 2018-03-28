184b65e59e61a2a89926935c10d3a3e3e4dc18d5
Andrew Weaver <weevz@uw.edu>
2018-03-28 14:01:50 -0700

    start to normalize column names to dc

--- contentdm_parse.rb
+++ contentdm_parse.rb
@@ -18,10 +18,43 @@ CSV.open("#{DesinationDIR}/metadata.csv", "wb") do |csv|
         filesearch = 'Path not found'
       end
     end
-    row ["Path"] = filesearch
+    row ["filename"] = filesearch
     if $. == 2
-      csv << row.headers
+      output_headers = Array.new
+      row.headers.each do |original_header|
+        if original_header == 'Title'
+          finalheader = 'dc.title'
+        elsif original_header == 'Date'
+          finalheader = 'dc.date'
+        elsif original_header == 'Subject'
+          finalheader = 'dc.subject'
+        elsif original_header == 'Creator'
+          finalheader = 'dc.creator'
+        elsif original_header == 'Description'
+          finalheader = 'dc.description'
+        elsif original_header == 'Rights'
+          finalheader = 'dc.rights'
+        elsif original_header == 'Type'
+          finalheader = 'dc.type'
+        elsif original_header == 'Identifier'
+          finalheader = 'dc.identifer'
+        elsif original_header == 'Source'
+          finalheader = 'dc.source'
+        elsif original_header == 'Publisher'
+          finalheader = 'dc.publisher'
+        elsif original_header == 'Language'
+          finalheader = 'dc.language'
+        elsif original_header == 'coverage'
+          finalheader = 'dc.coverage'
+        elsif original_header == 'Format'
+          finalheader = 'dc.format'
+        else
+          finalheader = original_header
+        end
+        output_headers << finalheader
+      end
+      csv << output_headers
     end
-    csv << row
+  csv << row
   end
 end
