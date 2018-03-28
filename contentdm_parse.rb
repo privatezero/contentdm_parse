0b8f81db30adef18d5884257279f2e6174dd8c90
Andrew Weaver <weevz@uw.edu>
2018-03-28 14:42:35 -0700

    more verbose

--- contentdm_parse.rb
+++ contentdm_parse.rb
@@ -4,18 +4,30 @@ require 'fileutils'
 InputCSV = ARGV[0]
 InputDIR = ARGV[1]
 DesinationDIR = ARGV[2]
-CSV.open("#{DesinationDIR}/metadata.csv", "wb") do |csv|
+if ! File.exists?("#{DesinationDIR}/objects")
+  Dir.mkdir "#{DesinationDIR}/objects"
+end
+if ! File.exists?("#{DesinationDIR}/metadata")
+  Dir.mkdir "#{DesinationDIR}/metadata"
+end
+CSV.open("#{DesinationDIR}/metadata/metadata.csv", "wb") do |csv|
   CSV.foreach(InputCSV,col_sep: "\t", quote_char: "ア",headers: true) do |row|
     if ! row['Identifier'].nil?
       id = File.basename(row['Identifier'])
-      id_path = Dir.glob("#{InputDIR}/**/#{id}")
+      id_path = Dir.glob("#{InputDIR}/**/#{id}.*")
       if ! id_path.empty?
         filesearch = id_path
         filesearch.each do |file|
-          FileUtils.cp file, DesinationDIR + '/' + File.basename(file)
+          if ! File.exist? "#{DesinationDIR}/objects" + '/' + File.basename(file)
+            puts "Copying: #{id_path}"
+            FileUtils.cp file, "#{DesinationDIR}/objects" + '/' + File.basename(file)
+          end
         end
       else
         filesearch = 'Path not found'
+        open("#{DesinationDIR}/missing_file_list.txt", 'a') do |missing|
+          missing << "#{File.basename(row['Identifier'])}.\n"
+        end
       end
     end
     row ["filename"] = filesearch
